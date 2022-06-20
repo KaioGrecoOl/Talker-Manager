f6e4685ea8b514a09f1c2842bc5dbf2d7e78da1f
Kaio G <kaiogreco91@gmail.com>
2022-06-20 20:36:44 -0300

    feat: cria rota talker

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const talkersList = require('./helpers/helper');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = '3000';
+const PORT = '3001';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.get('/talker', talkersList);
 
 app.listen(PORT, () => {
   console.log('Online');
